a8da8b0b8502026b85214099779e2e64389f0c55
bethanygresser <bethanygresser@users.noreply.github.com>
2015-05-15 16:31:30 -0400

    Update builder-form.js
    works

--- builder-form.js
+++ builder-form.js
@@ -1,17 +1,20 @@
 // function to take action based on value of radio button input
-function doThings(inputval) {
+function showSection(inputval) {
     if ( inputval == 'yes' )
-        alert('do things for YES!');
+        $(".company-references").toggle();
+        //alert(inputval);   
     if ( inputval == 'no' )
-        alert('Do things for No!');   
+        $(".company-references").hide();   
+        //alert(inputval);   
 }
 
-// call function doThings whenever the value of the radio button input is changed
+// call function showSection whenever the value of the radio button input is changed
 $('document').ready(
     function(){
-        $('input[name="gem_preferred"]:radio').click(
+        $('input[name$="gem_preferred"]:radio').click(
             function(){
-                doThings($('input[name="gem_preferred"]:checked').val());
+                showSection($('input[name="gem_preferred"]:checked').val());
+
             }
         );  
     }
